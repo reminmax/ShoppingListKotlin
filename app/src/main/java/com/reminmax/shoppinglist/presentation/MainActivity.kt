ccb4fba4e8f08eccaf2574bca2779999e17e98ae
reminmax <reminmax@gmail.com>
2022-05-28 04:53:35 +0300

    Second app screen

--- app/src/main/java/com/reminmax/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/reminmax/shoppinglist/presentation/MainActivity.kt
@@ -2,13 +2,11 @@ package com.reminmax.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.reminmax.shoppinglist.R
-import com.reminmax.shoppinglist.domain.ShopItem
 import com.reminmax.shoppinglist.presentation.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +24,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListadapter.submitList(it)
+        }
+
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -77,7 +81,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListadapter.shopItemClickListener = {
-            Log.d("MainActivity", it.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
