1209485ea1ded93c540beb18ea7a099646f879ef
reminmax <reminmax@gmail.com>
2022-05-30 03:45:27 +0300

    Add db model, dao and interface for Room

---
+++ app/src/main/java/com/reminmax/shoppinglist/data/AppdDatabase.kt
@@ -0,0 +1,43 @@
+package com.reminmax.shoppinglist.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1, exportSchema = false)
+abstract class AppdDatabase: RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object {
+
+        private var INSTANCE: AppdDatabase? = null
+        private val LOCK = Any()
+        private const val DB_NAME = "shop_items.db"
+
+        fun getInstance(application: Application): AppdDatabase {
+
+            INSTANCE?.let {
+                return it
+            }
+
+            synchronized(LOCK) {
+                INSTANCE?.let {
+                    return it
+                }
+            }
+            var db = Room.databaseBuilder(
+                application,
+                AppdDatabase::class.java,
+                DB_NAME
+            ).build()
+
+            INSTANCE = db
+
+            return db
+        }
+
+    }
+
+}
